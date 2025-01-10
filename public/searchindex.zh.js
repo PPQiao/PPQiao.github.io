4c0468472c3073b29ec3731015b2314883a9a663
QDS <854717881@qq.com>
2025-01-10 16:50:19 +0800

    第一次

---
+++ public/searchindex.zh.js
@@ -0,0 +1,26 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "EVE教程",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "类别",
+    "uri": "/zh/categories/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "EVE教程",
+    "uri": "/zh/index.html"
+  },
+  {
+    "breadcrumb": "EVE教程",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "标签",
+    "uri": "/zh/tags/index.html"
+  }
+]
